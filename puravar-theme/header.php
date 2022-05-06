21a4bde38d96519d2669648dcd3d4c70050b17dc
Juandiego Calero <88652170+Juandiegotron@users.noreply.github.com>
2022-05-06 17:33:16 +0200

    Fix header bug

--- puravar-theme/header.php
+++ puravar-theme/header.php
@@ -15,17 +15,29 @@
   src="https://code.jquery.com/jquery-3.6.0.min.js"
   integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
   crossorigin="anonymous"></script>
-	<script>
- 	 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new 	Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-57212093-1', 'auto');ga('send', 'pageview');
-	</script>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-57212093-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-57212093-1');
+</script>
+
 <?php wp_head(); ?>
 
 </head>
-<?php 	$classes = array();
-$btag = get_post_meta( $post->ID, 'tagbody', true );
-    if( ! empty( $btag ) ) {
-        $classes[] = $btag;
-    }
+<?php
+	$classes = array();
+	if (is_404()) {
+		$classes[] = 'grey-theme';
+	} else {
+		$btag = get_post_meta( $post->ID, 'tagbody', true );
+		if( ! empty( $btag ) ) {
+			$classes[] = $btag;
+		}
+	}
 ?>
 <body <?php body_class( $classes ); ?>>
 	<div class="main-top dark-theme ">
@@ -53,4 +65,4 @@ $btag = get_post_meta( $post->ID, 'tagbody', true );
 			<?php wp_nav_menu( array('container_class' => 'menu_nav main_menu', 'theme_location' => 'primary') ); ?>		
 		</div>
 	</header>
-		<div class="body">
+		<div class="main-body">
