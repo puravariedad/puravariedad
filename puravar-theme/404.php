eafb8ae080c694dd51960c855cfa91a2559302fc
Juandiego Calero <88652170+Juandiegotron@users.noreply.github.com>
2022-05-06 17:32:08 +0200

    Update 404

--- puravar-theme/404.php
+++ puravar-theme/404.php
@@ -1,12 +1,13 @@
 <?php get_header(); ?>
-	<div class="container row_fluid top-padded-big">
-		<section class="col grid8">
-			<div class="error">
-					<h1>Sorry man, nos perdimos</h1>
-					<strong>Regresar a la <a href="<?php echo home_url(); ?>"> home</a></strong>
-				</ul>
-			</div>
-		</section>
-		<?php get_sidebar(); ?>
+	<div class="container text-center top-padded-big bottom-padded">
+		<p>
+			<a href="<?php echo home_url(); ?>"><img src="https://puravariedad.com/wp-content/uploads/2022/05/headrest.gif" alt="Cabezaehuevo gif"></a>
+		</p>
+		<h1 class="top-padded">Error 404</h1>
+		<h2>¡Chanfle! la página que estás buscando no está disponible</h2>
+		<p>
+			Te recomendamos regresar a la home o enviar un mail a info@puravariedad.com
+		</p>
+		<a href="<?php echo home_url(); ?>" class="wp-block-button__link">Regresar a la home</a>
 	</div>	
 <?php get_footer(); ?>
