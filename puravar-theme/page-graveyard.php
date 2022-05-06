10cf09dfe664ec3017b25831172d37fd401ccf92
Juandiego Calero <88652170+Juandiegotron@users.noreply.github.com>
2022-05-06 17:37:21 +0200

    Erase extra class

--- puravar-theme/page-graveyard.php
+++ puravar-theme/page-graveyard.php
@@ -3,7 +3,7 @@
 template name: Graveyard
 */
 get_header(); ?>
-	<div class="container row_fluid top-padded-big">
+	<div class="container top-padded-big">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			 <div class="title bbottom_dash">
 				 <h1><?php the_title(); ?></h1>
